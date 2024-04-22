f9fb4f6c30f843d158e5a210a7555b54187c0c2b
Bauque Bernardita <bernibauque@gmail.com>
2024-04-21 22:18:24 -0300

    Arreglos finales a enqModel

--- models/enqModel.js
+++ models/enqModel.js
@@ -18,6 +18,11 @@ var enqSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    status: {
+        type: String,
+        default: "Submitted",
+        enum: ["Submitted", "Contacted", "In Progress"]
+    },
 });
 
 //Export the model
